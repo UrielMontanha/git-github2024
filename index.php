f069424bb1dd0ae8af0c4c1f6dbe0f59bc816cb6
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-03-04 17:00:10 -0300

    criei a tabela das informações

--- index.php
+++ index.php
@@ -6,23 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-
-<form method = "">
-
-    <fieldset>
-        <legend><h1>Login</h1></legend>
-    </fieldset>
-    
-    </form>
+    <table>
+        <thaed>
+            <tr>
+                <th>ID</th>
+                <th>Email</th>
+                <th cosplan="2" >opções</th>
+            </tr>
+        </thaed>
+        <tdbody>
+            <tr>
+                <td>1</td>
+                <td>Uriel</td>
+                <td>Aletrar</td>
+                <td>Excluir</td>
+            </tr>
+        </tdbody>
+    </table>
 </body>
 </html>
-
-
-<?PHP
-
-
-
-
-
-
-?>
