31ba923ea4f716f692af7be2c99c6add77044eaf
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-03-25 14:00:39 -0300

    refazendo index, começando o crood

--- index.php
+++ index.php
@@ -6,22 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <table>
-        <thaed>
-            <tr>
-                <th>ID</th>
-                <th>Email</th>
-                <th cosplan="2" >opções</th>
-            </tr>
-        </thaed>
-        <tdbody>
-            <tr>
-                <td>1</td>
-                <td>Uriel</td>
-                <td>Aletrar</td>
-                <td>Excluir</td>
-            </tr>
-        </tdbody>
-    </table>
+    
 </body>
 </html>
